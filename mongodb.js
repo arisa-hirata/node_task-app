5557c970aebec01d05f42504ea464326f23db020
“arisa-hirata” <arisa.pirata@gmail.com>
2021-01-17 22:17:22 -0800

    The ObjectID

--- mongodb.js
+++ mongodb.js
@@ -1,10 +1,14 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+const id = new ObjectID();
+console.log(id.id.length);
+console.log(id.toHexString().length);
+
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
@@ -14,8 +18,8 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     const db = client.db(databaseName);
 
     // db.collection('users').insertOne({
-    //     name: 'Arisa',
-    //     age: 31
+    //     name: 'Vikram',
+    //     age: 26
     // }, (error, result) => {
     //         if (error) {
     //             return console.log('Unable to insert user');
@@ -39,21 +43,21 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
     //         console.log(result.ops);
     // });
 
-    db.collection('tasks').insertMany([
-        {
-            description: 'find a job',
-            completed: false
-        }, {
-            description: 'accomplish the position',
-            completed: false
-        }, {
-            description: 'find your sparkle of life',
-            completed: false
-        }
-    ], (error, result) => {
-            if (error) {
-                return console.log('Unable to insert documents!');
-            }
-            console.log(result.ops);
-    });
+    // db.collection('tasks').insertMany([
+    //     {
+    //         description: 'find a job',
+    //         completed: false
+    //     }, {
+    //         description: 'accomplish the position',
+    //         completed: false
+    //     }, {
+    //         description: 'find your sparkle of life',
+    //         completed: false
+    //     }
+    // ], (error, result) => {
+    //         if (error) {
+    //             return console.log('Unable to insert documents!');
+    //         }
+    //         console.log(result.ops);
+    // });
 });
