d4d4be112f8d6374a0922c8d6694369bbd42a781
“arisa-hirata” <arisa.pirata@gmail.com>
2021-01-17 21:59:22 -0800

    Inserting Documents

---
+++ mongodb.js
@@ -0,0 +1,59 @@
+// CRUD create read update delete
+
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const connectionURL = 'mongodb://127.0.0.1:27017';
+const databaseName = 'task-manager';
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
+    if (error) {
+        return console.log('Unable to connect to database!');
+    }
+
+    const db = client.db(databaseName);
+
+    // db.collection('users').insertOne({
+    //     name: 'Arisa',
+    //     age: 31
+    // }, (error, result) => {
+    //         if (error) {
+    //             return console.log('Unable to insert user');
+    //         }
+
+    //         console.log(result.ops)
+    // });
+
+    // db.collection('users').insertMany([
+    //     {
+    //         name: 'Kiyomitsu',
+    //         age: 21
+    //     }, {
+    //         name: 'Yasusada',
+    //         age: 19
+    //     }
+    // ], (error, result) => {
+    //         if (error) {
+    //             return console.log('Unable to insert documents!');
+    //         }
+    //         console.log(result.ops);
+    // });
+
+    db.collection('tasks').insertMany([
+        {
+            description: 'find a job',
+            completed: false
+        }, {
+            description: 'accomplish the position',
+            completed: false
+        }, {
+            description: 'find your sparkle of life',
+            completed: false
+        }
+    ], (error, result) => {
+            if (error) {
+                return console.log('Unable to insert documents!');
+            }
+            console.log(result.ops);
+    });
+});
