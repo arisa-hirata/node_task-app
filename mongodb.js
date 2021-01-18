2c9cb9628d8b53381344ab39eb16589f955cfaf0
“arisa-hirata” <arisa.pirata@gmail.com>
2021-01-17 23:02:37 -0800

    Querying Documents

--- mongodb.js
+++ mongodb.js
@@ -5,11 +5,6 @@ const { MongoClient, ObjectID } = require('mongodb');
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
 
-const id = new ObjectID();
-console.log(id.id.length);
-console.log(id.toHexString().length);
-
-
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
     if (error) {
         return console.log('Unable to connect to database!');
@@ -17,47 +12,26 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
     const db = client.db(databaseName);
 
-    // db.collection('users').insertOne({
-    //     name: 'Vikram',
-    //     age: 26
-    // }, (error, result) => {
-    //         if (error) {
-    //             return console.log('Unable to insert user');
-    //         }
+    // db.collection('users').findOne({ _id: new ObjectID("60052657743abfa9a3253826") }, (error, user) => {
+    //     if (error) {
+    //         return console.log('Unable to fetch');
+    //     };
 
-    //         console.log(result.ops)
+    //     console.log(user);
     // });
 
-    // db.collection('users').insertMany([
-    //     {
-    //         name: 'Kiyomitsu',
-    //         age: 21
-    //     }, {
-    //         name: 'Yasusada',
-    //         age: 19
-    //     }
-    // ], (error, result) => {
-    //         if (error) {
-    //             return console.log('Unable to insert documents!');
-    //         }
-    //         console.log(result.ops);
+    // db.collection('users').find({ age: 31 }).toArray((error, users) => {
+    //     console.log(users)
     // });
 
-    // db.collection('tasks').insertMany([
-    //     {
-    //         description: 'find a job',
-    //         completed: false
-    //     }, {
-    //         description: 'accomplish the position',
-    //         completed: false
-    //     }, {
-    //         description: 'find your sparkle of life',
-    //         completed: false
-    //     }
-    // ], (error, result) => {
-    //         if (error) {
-    //             return console.log('Unable to insert documents!');
-    //         }
-    //         console.log(result.ops);
-    // });
+    db.collection('tasks').findOne({ _id: new ObjectID("600522fdfb3fe2a7db76af00") }, (error, task) => {
+        if (error) {
+            return console.log('Unable to fetch');
+        }
+        console.log(task);
+    });
+
+    db.collection('tasks').find({ completed: false }).toArray((error, tasks) => {
+        console.log(tasks);
+    });
 });
